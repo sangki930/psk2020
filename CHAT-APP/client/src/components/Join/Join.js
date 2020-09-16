0303d18012cba7659a3c0ca77f409b17f27add61
sangkipark <sangki931@gmail.com>
2020-09-16 11:55:19 +0900

    React 채팅 웹 커밋

--- CHAT-APP/client/src/components/Join/Join.js
+++ CHAT-APP/client/src/components/Join/Join.js
@@ -14,8 +14,8 @@ return (
     <div className="joinOuterContainer">
         <div className="joinInnerContainer">
             <h1 className="heading">Join</h1>
-            <div><input placeholder="Name" className="joinInput" onChange={(event)=>setName(event.target.value)} /></div>
-            <div><input placeholder="Room" className="joinInput mt-20" onChange={(event)=>setRoom(event.target.value)} /></div>
+            <div><input placeholder="이름" className="joinInput" onChange={(event)=>setName(event.target.value)} /></div>
+            <div><input placeholder="방 이름" className="joinInput mt-20" onChange={(event)=>setRoom(event.target.value)} /></div>
             <Link onClick={event=>(!name || !room)?event.preventDefault():null}to={`/chat?name=${name}&room=${room}`}>
                 <button className={"button mt-20"} type="submit">로그인</button>
             </Link>
